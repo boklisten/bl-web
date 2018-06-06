7974f31f3c3d9757c807cae31dee3bc3f64618cd
holskil <aholskil@gmail.com>
2018-06-06 13:14:22 +0200

    fix: header now displays cart and item select buttons on mobile

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -5,7 +5,7 @@ import {UserService} from "../user/user.service";
 
 export type MenuItem = 'info' | 'branch' | 'cart' | 'items'
 	| 'user-settings' | 'user-items' | 'user-orders' | 'logout'
-	| 'login' | 'register';
+	| 'login' | 'register' | 'user-home';
 
 @Component({
 	selector: 'app-header',
@@ -49,12 +49,16 @@ export class HeaderComponent implements OnInit {
 			this._router.navigateByUrl('/b/info/' + this._branchStoreService.getBranch().id);
 		} else if (menuItem === 'cart') {
 			this._router.navigateByUrl('/cart');
+		} else if (menuItem === 'info') {
+			this._router.navigateByUrl('/info');
 		} else if (menuItem === 'user-settings') {
 			this._router.navigateByUrl('/auth/register/detail');
 		} else if (menuItem === 'user-items') {
 			this._router.navigateByUrl('/u/items');
 		} else if (menuItem === 'user-orders') {
 			this._router.navigateByUrl('/u/order');
+		} else if (menuItem === 'user-home') {
+			this._router.navigateByUrl('/u/home');
 		} else if (menuItem === 'logout') {
 			this._userService.logout().then(() => {
 				this._router.navigateByUrl('welcome');
@@ -65,7 +69,7 @@ export class HeaderComponent implements OnInit {
 			this._router.navigateByUrl('/auth/register');
 		}
 
-		this.showMenu = !this.showMenu;
+		this.showMenu = false;
 	}
 
 	public onInfoClick() {
