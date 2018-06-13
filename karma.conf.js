21f445e6921ef97df85b82fd74336c8843572a54
holskil <aholskil@gmail.com>
2018-06-13 10:32:49 +0200

    build: removed bootstrap from angular-cli script imports

--- karma.conf.js
+++ karma.conf.js
@@ -2,32 +2,34 @@
 // https://karma-runner.github.io/1.0/config/configuration-file.html
 
 module.exports = function (config) {
-  config.set({
-    basePath: '',
-    frameworks: ['jasmine', '@angular/cli'],
-    plugins: [
-      require('karma-jasmine'),
-      require('karma-firefox-launcher'),
-      require('karma-jasmine-html-reporter'),
-      require('karma-coverage-istanbul-reporter'),
-      require('@angular/cli/plugins/karma')
-    ],
-    client:{
-      clearContext: false // leave Jasmine Spec Runner output visible in browser
-    },
-    coverageIstanbulReporter: {
-      reports: [ 'html', 'lcovonly' ],
-      fixWebpackSourcePaths: true
-    },
-    angularCli: {
-      environment: 'dev'
-    },
-    reporters: ['progress', 'kjhtml'],
-    port: 9876,
-    colors: true,
-    logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Firefox'],
-    singleRun: false
-  });
+	config.set({
+		basePath: './src/app/',
+		frameworks: ['jasmine', '@angular/cli'],
+		plugins: [
+			require('karma-jasmine'),
+			require('karma-firefox-launcher'),
+			require('karma-chrome-launcher'),
+			require('karma-jasmine-html-reporter'),
+			require('karma-coverage-istanbul-reporter'),
+			require('karma-phantomjs-launcher'),
+			require('@angular/cli/plugins/karma')
+		],
+		client: {
+			clearContext: false // leave Jasmine Spec Runner output visible in browser
+		},
+		coverageIstanbulReporter: {
+			reports: ['html', 'lcovonly'],
+			fixWebpackSourcePaths: true
+		},
+		angularCli: {
+			environment: 'dev'
+		},
+		reporters: ['progress', 'kjhtml'],
+		port: 9876,
+		colors: true,
+		logLevel: config.LOG_INFO,
+		autoWatch: true,
+		browsers: ['Firefox'],
+		singleRun: false
+	});
 };
