3f0a6ed315a81b8e9d8d7a4ee4128fdd4c4eaee2
holskil <aholskil@gmail.com>
2018-06-27 10:08:39 +0200

    feat: routes to policies rent, privacy and terms of service

--- src/app/info/info-routing.module.ts
+++ src/app/info/info-routing.module.ts
@@ -1,36 +1,28 @@
 import {NgModule} from '@angular/core';
 import {Routes, RouterModule} from '@angular/router';
 import {InfoComponent} from "./info.component";
-import {InfoAgreementComponent} from "./info-agreement/info-agreement.component";
 import {InfoAgreementPrivacyComponent} from "./info-agreement/info-agreement-privacy/info-agreement-privacy.component";
 import {InfoAgreementRentComponent} from "./info-agreement/info-agreement-rent/info-agreement-rent.component";
-import {InfoFaqComponent} from "./info-faq/info-faq.component";
+import {InfoAgreementTermsComponent} from "./info-agreement/info-agreement-terms/info-agreement-terms.component";
 
 const routes: Routes = [
 	{
 		path: 'info',
 		component: InfoComponent,
-		children: [
-			{
-				path: 'agreement',
-				component: InfoAgreementComponent,
-				children: [
-					{
-						path: 'privacy',
-						component: InfoAgreementPrivacyComponent
-					},
-					{
-						path: 'rent',
-						component: InfoAgreementRentComponent
-					}
-				]
-			},
-			{
-				path: 'faq',
-				component: InfoFaqComponent
-			}
-		]
+	},
+	{
+		path: 'info/policies/privacy',
+		component: InfoAgreementPrivacyComponent
+	},
+	{
+		path: 'info/policies/rent',
+		component: InfoAgreementRentComponent
+	},
+	{
+		path: 'info/policies/terms',
+		component: InfoAgreementTermsComponent
 	}
+
 ];
 
 @NgModule({
