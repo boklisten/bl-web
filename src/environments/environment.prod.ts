5e1bf8e78fde88fc7bb64ee29a8aaa18dfe8d9b1
holskil <aholskil@gmail.com>
2018-08-07 12:30:54 +0200

    fix: updated prod environment to include dibs variables

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
 export const environment = {
 	production: true,
-	apiPath: 'https://api.boklisten.no/'
+	apiPath: 'https://api.boklisten.no/',
+	dibs: {
+		checkoutKey: 'test-checkout-key-1a6981e849434c8c90dd382878b4310d',
+		language: 'nb-NO'
+	}
 };
