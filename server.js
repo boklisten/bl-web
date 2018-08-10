033c3bef8af179add272d9ea686a42259768f272
holskil <aholskil@gmail.com>
2018-08-11 00:48:59 +0200

    build: server to redirect to https in production environment

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ var path = require('path');
 
 
 
-app.use(express.static(__dirname + '/dist'));
 
 // If an incoming request uses
 // a protocol other than HTTPS,
 // redirect that request to the
 // same url but with HTTPS
+/*
 app.get('*', function(req, res, next) {
 	if (req.headers['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV === 'production') {
 		res.redirect('https://' + req.hostname + req.url);
@@ -18,13 +18,37 @@ app.get('*', function(req, res, next) {
 		next();
 	}
 });
+*/
+/*
+app.get('*', function(req, res, next) {
+	if (req.headers['x-forwarded-proto'] !== 'https') {
+		res.redirect('https://' + req.hostname + req.url);
+	} else {
+		next();
+	}
+});
+*/
+app.get('*', function (req, res, next) {
+	if (req.headers['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV === 'production') {
+		res.redirect('https://' + req.hostname + req.url);
+	} else {
+		next();
+	}
+});
+
+app.use(express.static(__dirname + '/dist'));
 
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
 // use this when you turn off the hash in angular, aka domain/#/endpoint
-app.all('*', function (req, res) {
-	res.status(200).sendFile('index.html', {root: __dirname + '/dist/'});
+app.get('*', function (req, res) {
+	if (req.headers['x-forwarded-proto'] !== 'https' && process.env.NODE_ENV === 'production') {
+		res.redirect('https://' + req.hostname + req.url);
+	} else {
+		res.sendFile(path.join(__dirname + '/dist/index.html'));
+	}
 });
+
 
 
 app.listen(process.env.PORT || 4200);
