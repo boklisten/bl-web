dd39cafcefcd762d2749db8f515509c3aa0faf54
holskil <aholskil@gmail.com>
2018-09-28 13:43:42 +0200

    fix: removed mention on Tanum on info pages

--- src/app/info/info-about/info-about.component.ts
+++ src/app/info/info-about/info-about.component.ts
@@ -19,7 +19,7 @@ export class InfoAboutComponent implements OnInit {
 			{
 				title: '',
 				textBlocks: [
-					'Boklisten.no (tidligere Søraas Bok) startet i 1990 med kjøp og salg av bøker til elever i videregående skole i Oslo og Akershus. Vi var involvert i utlånsordningen for skolebøker i 2000-01 og har i de senere årene samarbeidet med Tanum bokhandel i Oslo om brukte skolebøker. Vi administrerer også dagens utlånsordning for private og offentlige skoler i Oslo og har gode referanser herfra. Vi har bygget opp en betydelig kompetanse og erfaring, og anser oss som ledende innenfor salg og formidling av brukte skolebøker for videregående skoler.',
+					'Boklisten.no (tidligere Søraas Bok) startet i 1990 med kjøp og salg av bøker til elever i videregående skole i Oslo og Akershus. Vi var involvert i utlånsordningen for skolebøker i 2000-01. Vi administrerer også dagens utlånsordning for private og offentlige skoler i Oslo og har gode referanser herfra. Vi har bygget opp en betydelig kompetanse og erfaring, og anser oss som ledende innenfor salg og formidling av brukte skolebøker for videregående skoler.',
 					'I skoleboksesongen har vi 20-30 ansatte som har lang erfaring med behandling av store mengder bøker på kort tid.'
 				]
 			}
