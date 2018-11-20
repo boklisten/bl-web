a8f1635c3df45706ced878ecf88dc30cb23e55ad
holskil <aholskil@gmail.com>
2018-11-20 15:51:04 +0100

    build: dibs checkout script to be dynamicly imported

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import {environment} from './environments/environment';
 
 if (environment.production) {
 	enableProdMode();
+  // DIBS live checkout script
+  document.write('<script src="https://checkout.dibspayment.eu/v1/checkout.js?v=1"></script>');
+} else {
+  // DIBS test checkout script
+  document.write('<script src="https://test.checkout.dibspayment.eu/v1/checkout.js?v=1"></script>');
 }
 
 platformBrowserDynamic().bootstrapModule(AppModule)
