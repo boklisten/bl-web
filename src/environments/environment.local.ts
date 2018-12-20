d2eae45945c356ad23fa624e957589fcb54a2b44
holskil <aholskil@gmail.com>
2018-12-20 13:09:46 +0100

    build: added host flag for external browsers in dev mode

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -1,8 +1,8 @@
 export const environment = {
 	production: false,
-	apiPath: 'http://localhost:1337/',
+	apiPath: "192.168.0.65:1337/",
 	dibs: {
-		checkoutKey: 'test-checkout-key-1a6981e849434c8c90dd382878b4310d',
-		language: 'nb-NO'
+		checkoutKey: "test-checkout-key-1a6981e849434c8c90dd382878b4310d",
+		language: "nb-NO"
 	}
 };
