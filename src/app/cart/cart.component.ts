4c598646b7d6234f6b044f071257f3f7b81c7060
holskil <aholskil@gmail.com>
2019-01-10 01:28:36 +0100

    fix: user must be valid and logged in to do checkout

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { CartItem, CartService } from "./cart.service";
 import { CartOrderService } from "./cart-order/cart-order.service";
+import { UserService } from "../user/user.service";
 
 @Component({
 	selector: "app-cart",
@@ -8,16 +9,37 @@ import { CartOrderService } from "./cart-order/cart-order.service";
 	styleUrls: ["./cart.component.scss"]
 })
 export class CartComponent implements OnInit {
-	constructor(private _cartService: CartService) {}
+	public isUserValid: boolean;
+	public isUserLoggedIn: boolean;
+	constructor(
+		private _cartService: CartService,
+		private _userService: UserService
+	) {}
 
 	ngOnInit() {
 		if (this._cartService.isEmpty()) {
 			return;
 		}
+		this.isUserValid = true;
+		this.checkIfUserIsValid();
+		this.isUserLoggedIn = this._userService.loggedIn();
+	}
+
+	private checkIfUserIsValid() {
+		this._userService
+			.isUserDetailValid()
+			.then((valid: boolean) => {
+				this.isUserValid = valid;
+			})
+			.catch(() => {
+				this.isUserValid = false;
+			});
 	}
 
 	public showCart(): boolean {
-		return this.getCart().length > 0;
+		return (
+			this.getCart().length > 0 && this.isUserValid && this.isUserLoggedIn
+		);
 	}
 
 	public getCart(): CartItem[] {
