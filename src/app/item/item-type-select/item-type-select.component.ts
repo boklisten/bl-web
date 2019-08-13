2301121d6b73e17ee4406a4b0b295f323af2e5db
holskil <aholskil@gmail.com>
2019-08-13 15:14:01 +0200

    fix: item type select to show date for type 'rent'

--- src/app/item/item-type-select/item-type-select.component.ts
+++ src/app/item/item-type-select/item-type-select.component.ts
@@ -70,7 +70,7 @@ export class ItemTypeSelectComponent implements OnInit {
 	public getDate(action, period): Date {
 		if (action === "partly-payment") {
 			return this._dateService.getPartlyPaymentPeriodDate(period);
-		} else if (action === "semester" || action === "year") {
+		} else {
 			return this._dateService.getPeriodDate(period);
 		}
 	}
