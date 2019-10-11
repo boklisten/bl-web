f49f8d89e69b85eb7b544b0bee2cc2daa5865bdd
holskil <aholskil@gmail.com>
2019-10-11 11:57:41 +0200

    design: added a welcome-display component

--- src/environments/environment.local.ts
+++ src/environments/environment.local.ts
@@ -1,7 +1,7 @@
 export const environment = {
 	production: false,
 	hmr: true,
-	apiPath: "http://localhost:1337/",
+	apiPath: "http://10.27.101.162:1337/",
 	dibs: {
 		checkoutKey: "test-checkout-key-1a6981e849434c8c90dd382878b4310d",
 		language: "nb-NO"
