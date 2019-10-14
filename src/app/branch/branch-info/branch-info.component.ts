76ea105127d25849c514a24d97c505a08c894597
holskil <aholskil@gmail.com>
2019-10-14 16:34:55 +0200

    fix: alphabetical ordering of branch names in branch-info

--- src/app/branch/branch-info/branch-info.component.ts
+++ src/app/branch/branch-info/branch-info.component.ts
@@ -1,16 +1,15 @@
-import {Component, OnInit} from '@angular/core';
-import {ActivatedRoute, ParamMap, Router} from "@angular/router";
-import {BranchService} from "@wizardcoder/bl-connect";
-import {BlApiError, BlApiNotFoundError, Branch} from "@wizardcoder/bl-model";
-import {BranchStoreService} from "../branch-store.service";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, ParamMap, Router } from "@angular/router";
+import { BranchService } from "@wizardcoder/bl-connect";
+import { BlApiError, BlApiNotFoundError, Branch } from "@wizardcoder/bl-model";
+import { BranchStoreService } from "../branch-store.service";
 
 @Component({
-	selector: 'app-branch-info',
-	templateUrl: './branch-info.component.html',
-	styleUrls: ['./branch-info.component.scss']
+	selector: "app-branch-info",
+	templateUrl: "./branch-info.component.html",
+	styleUrls: ["./branch-info.component.scss"]
 })
 export class BranchInfoComponent implements OnInit {
-
 	public branch: Branch;
 	public warning: boolean;
 	public branches: Branch[];
@@ -18,28 +17,46 @@ export class BranchInfoComponent implements OnInit {
 	public showBranchMenu: boolean;
 	public loading: boolean;
 
-	constructor(private _branchService: BranchService, private _branchStoreService: BranchStoreService) {
+	constructor(
+		private _branchService: BranchService,
+		private _branchStoreService: BranchStoreService
+	) {
 		this.branches = [];
 	}
 
 	ngOnInit() {
-
-
 		this.selectedBranch = this._branchStoreService.getBranch();
 		this.loading = true;
 
-		this._branchService.get().then((branches: Branch[]) => {
-			this.branches = branches;
+		this._branchService
+			.get()
+			.then((branches: Branch[]) => {
+				this.branches = branches;
 
-			if (!this.selectedBranch) {
-				this.selectedBranch = this.branches[0];
-			}
+				this.branches.sort((a: any, b: any) => {
+					const nameA = a.name.toUpperCase();
+					const nameB = b.name.toUpperCase();
+					if (nameA < nameB) {
+						return -1;
+					} else if (nameA > nameB) {
+						return 1;
+					}
+					return 0;
+				});
 
-			this.loading = false;
-		}).catch((getBranchesError) => {
-			console.log('BranchInfoComponent: could not get brances', getBranchesError);
-			this.loading = false;
-		});
+				if (!this.selectedBranch) {
+					this.selectedBranch = this.branches[0];
+				}
+
+				this.loading = false;
+			})
+			.catch(getBranchesError => {
+				console.log(
+					"BranchInfoComponent: could not get brances",
+					getBranchesError
+				);
+				this.loading = false;
+			});
 	}
 
 	public onShowBranchMenu() {
