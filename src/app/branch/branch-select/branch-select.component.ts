e4848aa79e81cc9179f1fc7a55da0933a49c13c5
holskil <aholskil@gmail.com>
2019-10-14 16:30:32 +0200

    fix: alphabetical ordering of branch names in branch-select

--- src/app/branch/branch-select/branch-select.component.ts
+++ src/app/branch/branch-select/branch-select.component.ts
@@ -1,13 +1,13 @@
-import {Component, EventEmitter, OnInit, Output} from '@angular/core';
-import {BlApiError, Branch} from "@wizardcoder/bl-model";
-import {BranchStoreService} from "../branch-store.service";
-import {BranchService} from "@wizardcoder/bl-connect";
-import {Router} from "@angular/router";
+import { Component, EventEmitter, OnInit, Output } from "@angular/core";
+import { BlApiError, Branch } from "@wizardcoder/bl-model";
+import { BranchStoreService } from "../branch-store.service";
+import { BranchService } from "@wizardcoder/bl-connect";
+import { Router } from "@angular/router";
 
 @Component({
-	selector: 'app-branch-select',
-	templateUrl: './branch-select.component.html',
-	styleUrls: ['./branch-select.component.scss']
+	selector: "app-branch-select",
+	templateUrl: "./branch-select.component.html",
+	styleUrls: ["./branch-select.component.scss"]
 })
 export class BranchSelectComponent implements OnInit {
 	@Output() branchSelect: EventEmitter<Branch>;
@@ -17,9 +17,11 @@ export class BranchSelectComponent implements OnInit {
 	public loading: boolean;
 	public couldNotGetBranchesError: boolean;
 
-	constructor(private _branchService: BranchService,
-				private _branchStoreService: BranchStoreService,
-				private _router: Router) {
+	constructor(
+		private _branchService: BranchService,
+		private _branchStoreService: BranchStoreService,
+		private _router: Router
+	) {
 		this.branchSelect = new EventEmitter<Branch>();
 	}
 
@@ -30,13 +32,27 @@ export class BranchSelectComponent implements OnInit {
 
 		this.onBranchChange();
 
-		this._branchService.get().then((branches: Branch[]) => {
-			this.branches = branches;
-			this.loading = false;
-		}).catch((blApiError: BlApiError) => {
-			this.loading = false;
-			this.couldNotGetBranchesError = true;
-		});
+		this._branchService
+			.get()
+			.then((branches: Branch[]) => {
+				this.branches = branches;
+
+				this.branches.sort((a: any, b: any) => {
+					const nameA = a.name.toUpperCase();
+					const nameB = b.name.toUpperCase();
+					if (nameA < nameB) {
+						return -1;
+					} else if (nameA > nameB) {
+						return 1;
+					}
+					return 0;
+				});
+				this.loading = false;
+			})
+			.catch((blApiError: BlApiError) => {
+				this.loading = false;
+				this.couldNotGetBranchesError = true;
+			});
 	}
 
 	private onBranchChange() {
@@ -57,10 +73,7 @@ export class BranchSelectComponent implements OnInit {
 			this._router.navigateByUrl(this._branchStoreService.redirectUrl);
 			this._branchStoreService.redirectUrl = null;
 		} else {
-			this._router.navigateByUrl('/i/select');
+			this._router.navigateByUrl("/i/select");
 		}
 	}
-
-
-
 }
