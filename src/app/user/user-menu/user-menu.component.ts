4a583102ddbd7eb9097a87f948cf46a63c3c6475
holskil <aholskil@gmail.com>
2019-10-15 13:24:18 +0200

    fix: updated paths in user-menu

--- src/app/user/user-menu/user-menu.component.ts
+++ src/app/user/user-menu/user-menu.component.ts
@@ -1,12 +1,12 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {MenuItem} from "../../header/header.component";
-import {Router} from "@angular/router";
-import {UserService} from "../user.service";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { MenuItem } from "../../header/header.component";
+import { Router } from "@angular/router";
+import { UserService } from "../user.service";
 
 @Component({
-	selector: 'app-user-menu',
-	templateUrl: './user-menu.component.html',
-	styleUrls: ['./user-menu.component.scss']
+	selector: "app-user-menu",
+	templateUrl: "./user-menu.component.html",
+	styleUrls: ["./user-menu.component.scss"]
 })
 export class UserMenuComponent implements OnInit {
 	@Input() showMenu: boolean;
@@ -16,36 +16,35 @@ export class UserMenuComponent implements OnInit {
 		this.showMenuChange = new EventEmitter<boolean>();
 	}
 
-	ngOnInit() {
-	}
+	ngOnInit() {}
 
 	public onMenuItemClick(menuItem: MenuItem) {
-		if (menuItem === 'items') {
-			this._router.navigateByUrl('/i/select');
-		} else if (menuItem === 'branch') {
-			this._router.navigateByUrl('/b/info');
-		} else if (menuItem === 'cart') {
-			this._router.navigateByUrl('/cart');
-		} else if (menuItem === 'info') {
-			this._router.navigateByUrl('/info');
-		} else if (menuItem === 'user-settings') {
-			this._router.navigateByUrl('/u/edit');
-		} else if (menuItem === 'user-items') {
-			this._router.navigateByUrl('/u/items');
-		} else if (menuItem === 'user-orders') {
-			this._router.navigateByUrl('/u/order');
-		} else if (menuItem === 'user-home') {
-			this._router.navigateByUrl('/u/home');
-		} else if (menuItem === 'logout') {
+		if (menuItem === "items") {
+			this._router.navigateByUrl("/i/select");
+		} else if (menuItem === "branch") {
+			this._router.navigate(["/info/branch"]);
+		} else if (menuItem === "cart") {
+			this._router.navigateByUrl("/cart");
+		} else if (menuItem === "info") {
+			this._router.navigate(["/info"]);
+		} else if (menuItem === "user-settings") {
+			this._router.navigateByUrl("/u/edit");
+		} else if (menuItem === "user-items") {
+			this._router.navigateByUrl("/u/items");
+		} else if (menuItem === "user-orders") {
+			this._router.navigateByUrl("/u/order");
+		} else if (menuItem === "user-home") {
+			this._router.navigateByUrl("/u/home");
+		} else if (menuItem === "logout") {
 			this._userService.logout().then(() => {
-				this._router.navigateByUrl('logout');
+				this._router.navigateByUrl("logout");
 			});
-		} else if (menuItem === 'login') {
-			this._router.navigateByUrl('/auth/login');
-		} else if (menuItem === 'register') {
-			this._router.navigateByUrl('/auth/register');
-		} else if (menuItem === 'home') {
-			this._router.navigateByUrl('/');
+		} else if (menuItem === "login") {
+			this._router.navigateByUrl("/auth/login");
+		} else if (menuItem === "register") {
+			this._router.navigateByUrl("/auth/register");
+		} else if (menuItem === "home") {
+			this._router.navigateByUrl("/");
 		}
 
 		this.onMenuClick();
@@ -58,5 +57,4 @@ export class UserMenuComponent implements OnInit {
 	public isLoggedIn() {
 		return this._userService.loggedIn();
 	}
-
 }
