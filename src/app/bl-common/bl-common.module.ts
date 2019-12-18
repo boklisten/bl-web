41563e005d1e0b0b9c8068627ec8e03f4245168c
holskil <andreasholskil@protonmail.com>
2019-12-18 17:11:46 +0100

    fix: deleted unused directive blc-click-outside

--- src/app/bl-common/bl-common.module.ts
+++ src/app/bl-common/bl-common.module.ts
@@ -1,19 +1,16 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
-import {BlcPricePipe} from './pipes/blc-price/blc-price.pipe';
-import { BlcDatePipe } from './pipes/blc-date/blc-date.pipe';
-import { BlcBranchPipe } from './pipes/blc-branch/blc-branch.pipe';
-import { BlcItemPricePipe } from './pipes/blc-item-price/blc-item-price.pipe';
-import { BlcCustomerItemPricePipe } from './pipes/blc-customer-item-price/blc-customer-item-price.pipe';
-import { BlcCustomerItemDatePipe } from './pipes/blc-customer-item-date/blc-customer-item-date.pipe';
-import { BlcSpinnerComponent } from './components/blc-spinner/blc-spinner.component';
-import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { BlcPricePipe } from "./pipes/blc-price/blc-price.pipe";
+import { BlcDatePipe } from "./pipes/blc-date/blc-date.pipe";
+import { BlcBranchPipe } from "./pipes/blc-branch/blc-branch.pipe";
+import { BlcItemPricePipe } from "./pipes/blc-item-price/blc-item-price.pipe";
+import { BlcCustomerItemPricePipe } from "./pipes/blc-customer-item-price/blc-customer-item-price.pipe";
+import { BlcCustomerItemDatePipe } from "./pipes/blc-customer-item-date/blc-customer-item-date.pipe";
+import { BlcSpinnerComponent } from "./components/blc-spinner/blc-spinner.component";
+import { FontAwesomeModule } from "@fortawesome/angular-fontawesome";
 
 @NgModule({
-	imports: [
-		CommonModule,
-		FontAwesomeModule
-	],
+	imports: [CommonModule, FontAwesomeModule],
 	declarations: [
 		BlcPricePipe,
 		BlcDatePipe,
@@ -33,5 +30,4 @@ import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
 		BlcSpinnerComponent
 	]
 })
-export class BlCommonModule {
-}
+export class BlCommonModule {}
--- src/app/bl-common/directives/blc-click-outside/blc-click-outside.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { BlcClickOutsideDirective } from './blc-click-outside.directive';
-
-describe('BlcClickOutsideDirective', () => {
-  it('should create an instance', () => {
-    const directive = new BlcClickOutsideDirective();
-    expect(directive).toBeTruthy();
-  });
-});
