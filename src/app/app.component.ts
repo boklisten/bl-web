d9b417a0f21b0308c5907944509abed8f02a4ac5
holskil <andreasholskil@protonmail.com>
2019-12-20 15:20:42 +0100

    fix: header alert to only be displayed on certain pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,22 +9,45 @@ import { ActivatedRoute, Router } from "@angular/router";
 export class AppComponent {
 	title = "app";
 	public showGutter: boolean = true;
+	public showAlert: boolean = false;
 
 	constructor(private _router: Router, private route: ActivatedRoute) {}
 
 	ngOnInit() {
 		this.checkToShowGutter();
+		this.checkShowAlert();
 
 		this._router.events.subscribe(() => {
 			this.checkToShowGutter();
+			this.checkShowAlert();
 			window.scroll(0, 0);
 		});
 	}
 
 	private checkToShowGutter() {
-		if (this.route.snapshot.firstChild) {
+		if (
+			this.route &&
+			this.route.snapshot &&
+			this.route.snapshot.firstChild
+		) {
 			this.showGutter =
 				this.route.snapshot.firstChild.url[0].path !== "welcome";
+		}
+	}
+
+	private checkShowAlert() {
+		if (
+			this.route &&
+			this.route.snapshot &&
+			this.route.snapshot.firstChild
+		) {
+			const path: string = this.route.snapshot.firstChild.url[0].path;
+			this.showAlert = !(
+				path === "cart" ||
+				path === "u" ||
+				path === "i" ||
+				path === "fastbuy"
+			);
 		}
 	}
 }
