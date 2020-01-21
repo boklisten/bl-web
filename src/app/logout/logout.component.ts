751ee88c687d199da5a653a773bcacf3387269ba
holskil <andreasholskil@protonmail.com>
2020-01-21 17:14:26 +0100

    design: added container-fluid to some components

--- src/app/logout/logout.component.ts
+++ src/app/logout/logout.component.ts
@@ -1,20 +1,17 @@
-import {Component, OnInit} from '@angular/core';
-import {Router} from "@angular/router";
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-	selector: 'app-logout',
-	templateUrl: './logout.component.html',
-	styleUrls: ['./logout.component.scss']
+	selector: "app-logout",
+	templateUrl: "./logout.component.html",
+	styleUrls: ["./logout.component.scss"]
 })
 export class LogoutComponent implements OnInit {
-
-	constructor(private _router: Router) {
-	}
+	constructor(private _router: Router) {}
 
 	ngOnInit() {
 		setTimeout(() => {
-			this._router.navigate(['welcome']);
+			this._router.navigate(["welcome"]);
 		}, 2500);
 	}
-
 }
